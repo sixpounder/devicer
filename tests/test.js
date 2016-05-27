d1baa3875851d0d91eb32b012485d0a919d88922
Andrea Coronese <sixpounder@protonmail.com>
2016-05-27 12:19:21 +0200

    more tests, small fixes

--- tests/test.js
+++ tests/test.js
@@ -3,9 +3,14 @@ var assert = require('assert');
 var devicer = require('../');
 
 var chromeOnOsx = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
+
 var android = "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19";
 var ipad = "Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10";
 var iphone = "Mozilla/5.0 (iPhone; CPU iPhone OS 6_1_3 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10B329 Safari/8536.25";
+
+var edge = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246";
+var ie   = "Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko";
+var firefox = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1";
 
 describe("devicer", function() {
   describe('basic functionalities', function() {
@@ -58,6 +63,32 @@ describe("devicer", function() {
       assert.notEqual("", obj.build);
       assert.notEqual(undefined , obj.engine);
       assert.notEqual("", obj.additional);
+    });
+  });
+
+  describe('browser detection', function() {
+    it('correctly detects edge', function() {
+      var obj = devicer.parseUserAgent(edge);
+      assert.notEqual(null, obj);
+      assert.equal(true, obj.engine.isIE());
+    });
+
+    it('correctly detects IE', function() {
+      var obj = devicer.parseUserAgent(ie);
+      assert.notEqual(null, obj);
+      assert.equal(true, obj.engine.isIE());
+    });
+
+    it('correctly detects Firefox', function() {
+      var obj = devicer.parseUserAgent(firefox);
+      assert.notEqual(null, obj);
+      assert.equal(true, obj.engine.isFirefox());
+    });
+
+    it('correctly detects Chrome', function() {
+      var obj = devicer.parseUserAgent(chromeOnOsx);
+      assert.notEqual(null, obj);
+      assert.equal(true, obj.engine.isChrome());
     });
   });
 
